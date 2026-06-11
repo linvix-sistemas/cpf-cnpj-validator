5cb60f87515c767d96243699fac739adffd371dd
patrick-linvix <dev13@linvix.com.br>
2026-06-11 14:33:02 -0300

    fix: removed BlackList

--- src/cnpj.ts
+++ src/cnpj.ts
@@ -1,15 +1,6 @@
 // Blacklist common values.
 const BLACKLIST: Array<string> = [
   "00000000000000",
-  "11111111111111",
-  "22222222222222",
-  "33333333333333",
-  "44444444444444",
-  "55555555555555",
-  "66666666666666",
-  "77777777777777",
-  "88888888888888",
-  "99999999999999",
 ];
 
 // Strict: remove only mask separators. Loose: remove anything that is not an alphanumeric char.
